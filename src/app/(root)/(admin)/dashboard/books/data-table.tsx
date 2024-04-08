6316ceb6cb887f94f4a48ffb2e121b425e4199ef
Gaks3 <dwi66116@gmail.com>
2024-04-09 00:24:08 +0700

    feat: book table component

---
+++ src/app/(root)/(admin)/dashboard/books/data-table.tsx
@@ -0,0 +1,221 @@
+'use client'
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from '@tanstack/react-table'
+import { useState } from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Button } from '@/components/ui/button'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { TableProperties } from 'lucide-react'
+import Link from 'next/link'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [sorting, setSorting] = useState<SortingState>([])
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({
+    language: false,
+    year_published: false,
+    page: false,
+  })
+  const [searchBy, setSearchBy] = useState('title')
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      columnFilters,
+      sorting,
+      columnVisibility,
+    },
+  })
+
+  return (
+    <ScrollArea className='sm:w-[93vw] md:w-full'>
+      <div className='space-y-5'>
+        <div className='flex justify-between max-h-10'>
+          <div className='flex h-10 gap-5'>
+            <div className='flex items-center p-1 border rounded-md w-fit border-border'>
+              <input
+                type='text'
+                placeholder='Search...'
+                className='flex-1 h-full px-3 text-sm border-none outline-none placeholder:text-muted-foreground'
+                value={table.getColumn(searchBy)?.getFilterValue() as string}
+                onChange={(event) =>
+                  table.getColumn(searchBy)?.setFilterValue(event.target.value)
+                }
+              />
+              <Select
+                value={searchBy}
+                onValueChange={(value) => setSearchBy(value)}
+                defaultValue='title'
+              >
+                <SelectTrigger className='!outline-none !border-none !ring-0 !ring-offset-0 w-fit bg-secondary gap-3 capitalize h-8 rounded'>
+                  <SelectValue placeholder='Search By' />
+                </SelectTrigger>
+                <SelectContent>
+                  {table
+                    .getAllColumns()
+                    .filter((column) => column.getIsVisible())
+                    .filter((column) => column.getCanFilter())
+                    .map((column) => (
+                      <SelectItem
+                        key={column.id}
+                        value={column.id}
+                        className='capitalize'
+                      >
+                        {column.id.replace('_', ' ')}
+                      </SelectItem>
+                    ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='outline' className='flex gap-2 ml-auto'>
+                  <TableProperties size={18} />
+                  Columns
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                {table
+                  .getAllColumns()
+                  .filter((column) => column.getCanHide())
+                  .map((column) => {
+                    return (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className='capitalize'
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) =>
+                          column.toggleVisibility(!!value)
+                        }
+                      >
+                        {column.id.replace('_', ' ')}
+                      </DropdownMenuCheckboxItem>
+                    )
+                  })}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <Button asChild>
+            <Link href={'/dashboard/books/add'}>Add Book</Link>
+          </Button>
+        </div>
+        <div className='border rounded-md'>
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    )
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && 'selected'}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className='h-24 text-center'
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className='flex items-center justify-center py-4 space-x-2'>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+      <ScrollBar orientation='horizontal' />
+    </ScrollArea>
+  )
+}
