6c4ba8b60db71481b96eaba8c7270ec1b4d7e4e7
Gaks3 <dwi66116@gmail.com>
2024-04-11 23:36:31 +0700

    feat: upload an image server action

---
+++ src/lib/actions/image.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import { writeFile, unlink } from 'fs/promises'
+import { join } from 'path'
+import slug from 'slug'
+
+export const upload = async (data: FormData) => {
+  const sizeLimit = 10_000_000
+
+  const file: File | null = data.get('image') as unknown as File
+
+  if (!file.type.includes('image')) throw new Error('Please upload an image')
+  if (file.size > sizeLimit) throw new Error('Max size 10MB')
+  if (!file) throw new Error('Something field are missing')
+
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
+
+  const folderPathName = 'Code/belajar/lks/perpus/public'
+
+  const randomString = crypto.randomUUID()
+
+  const fileName = `${randomString.slice(0, 3)}-${file.name}`
+
+  const path = join(
+    '/',
+    folderPathName,
+    slug(fileName, { replacement: '-', lower: true })
+  )
+  await writeFile(path, buffer)
+
+  return { url: '/' + fileName }
+}
