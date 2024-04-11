1f34e5d4098776ca95751daceb922ede2869d22b
Gaks3 <dwi66116@gmail.com>
2024-04-11 23:31:55 +0700

    feat: get list of key function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,16 @@ export const containsNumber = (str: string) => /\d/.test(str)
 
 export const containsSpecialCharacter = (str: string) =>
   /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(str)
+
+export const getKeys = (data: Array<{ [key: string]: any }>) => {
+  const combinedObject = data.reduce((result, obj) => {
+    for (const key in obj) {
+      result[key] = obj[key]
+    }
+    return result
+  }, {})
+
+  const uniqueKeys = Array.from(new Set(Object.keys(combinedObject)))
+
+  return uniqueKeys
+}
