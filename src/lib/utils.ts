be4fba147ea8bffd2fa64a636f56589f3e07a279
Gaks3 <dwi66116@gmail.com>
2024-04-11 23:31:29 +0700

    feat: validation regex for password

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const containsUppercase = (str: string) => /[A-Z]/.test(str)
+
+export const containsNumber = (str: string) => /\d/.test(str)
+
+export const containsSpecialCharacter = (str: string) =>
+  /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(str)
