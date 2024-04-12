d15214e9bd49a55d73a9d50445afaa5376421f3c
Gaks3 <dwi66116@gmail.com>
2024-04-12 17:20:08 +0700

    feat: delete user server action

--- src/lib/actions/user.ts
+++ src/lib/actions/user.ts
@@ -1,11 +1,14 @@
 'use server'
 
-import { getUser } from '../auth'
+import { Argon2id } from 'oslo/password'
+import { getUser, lucia } from '../auth'
 import prisma from '../db'
+import { revalidatePath } from 'next/cache'
 
 export type UserUpdate = {
   username?: string
   email?: string
+  password?: string
   isAdmin?: boolean
 }
 
@@ -14,4 +17,21 @@ export async function getUsers() {
   if (!user || !user.user?.isAdmin) throw new Error('Unauthorized')
 
   return await prisma.user.findMany()
+}
+
+export async function deleteUser(id: string) {
+  const user = await getUser()
+  if (!user || !user.user?.isAdmin) throw new Error('Unauthorized')
+
+  await lucia.deleteExpiredSessions()
+
+  const res = await prisma.user.delete({
+    where: {
+      id,
+    },
+  })
+
+  revalidatePath('/dashboard/user')
+
+  return res
 }
